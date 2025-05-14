6b795b513b0d2d155d3d23bb4d0db60393343158
AlexAlarcon <125610346+HALEXALARCON@users.noreply.github.com.>
2025-05-14 16:28:04 -0500

    first entregable

---
+++ src/data/postgres/models/user.model.ts
@@ -0,0 +1,58 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+export enum userRole {
+  USER = "user",
+  ADMIN = "admin"
+}
+
+
+@Entity()
+export class user extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column("varchar", {
+    length: 150,
+  })
+
+  name: string;
+
+  @Column("varchar", {
+    length: 150,
+    unique: true,
+    nullable: false,
+  })
+
+  email: string;
+
+  @Column("varchar", {
+    length: 255,
+    nullable: false,
+  })
+
+  password: string;
+
+  @Column("enum", {
+    enum: userRole,
+    default: userRole.USER,
+  })
+
+  user: userRole;
+
+  @Column("boolean", {
+    nullable: false,
+    default: true,
+  })
+
+  status: boolean;
+
+  @Column('timestamp', {
+    default: () => 'CURRENT_TIMESTAMP',
+    nullable: false,
+  })
+
+  createt_at: Date;
+
+}
+
